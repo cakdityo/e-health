7a7a55b036f9b15c9ce3698dff3f637be51277ad
Adityo Rancaka <cakdityo@gmail.com>
2016-07-01 16:31:30 +0700

    create database migrations

---
+++ database/migrations/2016_07_01_090753_create_diseases_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiseasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diseases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('disease_indication', function (Blueprint $table){
+            $table->integer('disease_id')->unsigned();
+            $table->integer('indication_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('disease_id')->references('id')->on('diseases')->onDelete('cascade');
+            $table->foreign('indication_id')->references('id')->on('indications')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('diseases');
+    }
+}
---
+++ database/migrations/2016_07_01_090824_create_diagnosis_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiagnosisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diagnosis', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('disease_id')->unsigned();
+            $table->float('cf_total');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('disease_id')->references('id')->on('diseases')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('diagnosis_indication', function (Blueprint $table){
+            $table->integer('diagnosis_id')->unsigned();
+            $table->integer('indication_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('diagnosis_id')->references('id')->on('diagnosis')->onDelete('cascade');
+            $table->foreign('indication_id')->references('id')->on('indications')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diagnosis_indication');
+        Schema::drop('diagnosis');
+    }
+}
