dde38b02791bd8e98ad5563b58c6515e9b89eeb0
Adityo Rancaka <cakdityo@gmail.com>
2016-07-02 14:41:11 +0700

    count cf

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\Disease;
+use App\Indication;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 
@@ -27,6 +28,24 @@ class HomeController extends Controller
     public function index()
     {
         $user = Auth::user();
-        return view('home', compact('user'));
+        $indications = Indication::all();
+        return view('home', compact('user','indications'));
+    }
+
+    public function store(Request $request)
+    {
+        $diseases = [];
+        $userIndications = $request->input('indications');
+        foreach($userIndications as $userIndication){
+            $tempDiseases = Indication::find($userIndication)->diseases;
+            foreach($tempDiseases as $dis){
+                if(array_has($diseases, $dis->id)){
+                    $diseases[$dis->id] = $diseases[$dis->id] + $dis->pivot->cf_score;
+                } else {
+                    $diseases[$dis->id] = $dis->pivot->cf_score;
+                }
+            }
+        }
+        return $diseases;
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,21 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <strong>Hello</strong> {{ $user->name }}!
                 </div>
             </div>
-            {{ $user }}
+            <br>
+            <form method="post" action="{{ URL::to('/diagnosis') }}">
+                <div class="list-group">
+                    @foreach($indications as $indication)
+                        <div class="list-group-item">
+                            {{ $indication->name }} <input class="pull-right" type="checkbox" name="indications[]" value="{{ $indication->id }}">
+                        </div>
+                    @endforeach
+                </div>
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input class="form-control btn btn-primary" type="submit" value="Submit">
+            </form>
         </div>
     </div>
 </div>
